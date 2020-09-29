ca9d0e593d4e695cdeee128b51de55e48038215d
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-09-29 16:09:02 +0300

    mock data change

--- app/mock/users.mock.js
+++ app/mock/users.mock.js
@@ -1,14 +1,14 @@
 const users = [
     {
         userId : 1,
-        displayName : 'Toprak E.',
-        email : 'toprak@toprak.com',
+        displayName : 'Test User 1',
+        email : 'test@test.com',
         password : '123456'
     },
     {
         userId : 2,
-        displayName : 'Test User',
-        email : 'test@test.com',
+        displayName : 'Test User 2',
+        email : 'test2@test.com',
         password : '123123'
     }
 ];
