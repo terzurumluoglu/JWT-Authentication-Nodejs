01f9ee9086ac4b13069ca7ac151f7991c9eaaae8
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2022-04-02 00:01:07 +0300

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-const routes = require('./routes');
+const routes = require('./app/routes');
 app.use('/api',routes);
 
 //start http server
